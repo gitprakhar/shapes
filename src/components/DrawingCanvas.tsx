6523de208db8c0a6f744f6ad6709e1029225c81c
Prakhar Mittal <prakharm1@gmail.com>
2026-01-24 12:46:14 -0500

    drawing canvas now has the same padding as the homepage on mobile

--- src/components/DrawingCanvas.tsx
+++ src/components/DrawingCanvas.tsx
@@ -1109,7 +1109,7 @@ export function DrawingCanvas({ onSubmit }: DrawingCanvasProps) {
         />
 
         {/* Bottom section: text left with buttons below, moves right */}
-        <div className="absolute bottom-16 left-0 right-0 z-20 px-12 pointer-events-none">
+        <div className="absolute left-0 right-0 z-20 px-12 pointer-events-none" style={{ bottom: 'calc(4rem + env(safe-area-inset-bottom))' }}>
           <div className="flex items-end justify-between">
             {/* Left: Text and buttons */}
             <div className="flex flex-col">
